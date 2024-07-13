65f33d477d58422fa0cdb3e73075294330ea42aa
faketuna <faketuna.st@gmail.com>
2024-07-13 23:28:04 +0900

    chore: initial commit

---
+++ CSSMapChooser/CSSMapChooser.cs
@@ -0,0 +1,28 @@
+﻿using CounterStrikeSharp.API.Core;
+
+namespace CSSMapChooser;
+
+public class CSSMapChooser : BasePlugin
+{
+    public override string ModuleName => "CounterStrikeSharp Map Chooser";
+
+    public override string ModuleVersion => "0.0.1";
+
+    public override string ModuleAuthor => "faketuna";
+
+    public override string ModuleDescription => "CounterStrikeSharp implementation of map chooser";
+
+
+
+
+    public override void Load(bool hotReload)
+    {
+        
+    }
+
+    public override void Unload(bool hotReload)
+    {
+        
+    }
+
+}
