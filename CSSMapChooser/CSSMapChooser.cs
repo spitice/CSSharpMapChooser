98e01e39db4b789a8e3d939bc412d2cc4ecd2296
faketuna <faketuna.st@gmail.com>
2024-07-15 03:24:48 +0900

    feat: add timeleft command

--- CSSMapChooser/CSSMapChooser.cs
+++ CSSMapChooser/CSSMapChooser.cs
@@ -1,5 +1,7 @@
+using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Modules.Commands;
+using CounterStrikeSharp.API.Modules.Cvars;
 using CounterStrikeSharp.API.Modules.Utils;
 using Microsoft.Extensions.Logging;
 
@@ -19,6 +21,10 @@ public class CSSMapChooser : BasePlugin
 
     private MapData nextMapData = default!;
 
+    private ConVar? mp_timelimit = null;
+
+    private int timeleft = 0;
+
     private readonly string CHAT_PREFIX = $" {ChatColors.Green}[CSSMC]{ChatColors.Default}";
 
     public override void Load(bool hotReload)
@@ -31,8 +37,22 @@ public class CSSMapChooser : BasePlugin
         Logger.LogInformation("Initializing the Next map data");
         nextMapData = new MapData("NONE", false);
 
+        Logger.LogInformation("Registering timeleft calculation timer.");
+        RegisterListener<Listeners.OnTick>(() => {
+            if (GetGameRules() != null && mp_timelimit != null)
+				timeleft = (int)((GetGameRules().GameStartTime + mp_timelimit.GetPrimitiveValue<float>() * 60.0f) - Server.CurrentTime);
+			else if (mp_timelimit == null) {
+				Logger.LogWarning("Failed to find the mp_timelimit ConVar and try to find again.");
+				mp_timelimit = ConVar.Find("mp_timelimit");
+			}
+			else {
+				Logger.LogError("Failed to find the Game Rules entity!");
+			}
+        });
+
         Logger.LogInformation("Adding commands...");
         AddCommand("css_nextmap", "shows nextmap information", CommandNextMap);
+        AddCommand("css_timeleft", "shows current map limit time", CommandTimeLeft);
     }
 
     public override void OnAllPluginsLoaded(bool hotReload)
@@ -56,4 +76,42 @@ public class CSSMapChooser : BasePlugin
     private void ShowNextMapInfo(CCSPlayerController client) {
         client.PrintToChat($"{CHAT_PREFIX} Next map: {nextMapData.MapName}");
     }
+
+
+
+    private void CommandTimeLeft(CCSPlayerController? client, CommandInfo info) {
+        if(client == null)
+            return;
+        
+        ShowTimeLeft(client);
+    }
+
+    private void ShowTimeLeft(CCSPlayerController client) {
+        if(timeleft < 1) {
+            client.PrintToChat($"{CHAT_PREFIX} Last round!");
+        }
+        else {
+            client.PrintToChat($"{CHAT_PREFIX} Timeleft: {GetFormattedTimeLeft(timeleft)}");
+        }
+    }
+
+    private string GetFormattedTimeLeft(int timeleft) {
+        int minutes = timeleft / 60;
+        int seconds = timeleft % 60;
+        string formatted = "";
+
+        if(minutes > 0) {
+            formatted = $"{minutes} minutes {seconds} seconds";
+        }
+        else {
+            formatted = $"{seconds} seconds";
+        }
+
+        return formatted;
+    }
+
+    private CCSGameRules GetGameRules()
+	{
+		return Utilities.FindAllEntitiesByDesignerName<CCSGameRulesProxy>("cs_gamerules").First().GameRules!;
+	}
 }
