cc5ee23e4ed6b345291401bd7896e243f992ba41
faketuna <faketuna.st@gmail.com>
2024-07-16 05:11:09 +0900

    fix: Fix error when simple logging invoked in server startup

--- CSSMapChooser/SimpleLogging.cs
+++ CSSMapChooser/SimpleLogging.cs
@@ -3,23 +3,29 @@ using CounterStrikeSharp.API;
 namespace CSSMapChooser;
 
 public static class SimpleLogging {
-        public static void LogDebug(string information) {
+    public static void LogDebug(string information) {
+        try {
             foreach(var client in Utilities.GetPlayers()) {
                 if(!client.IsValid || client.IsBot || client.IsHLTV)
                     continue;
                 
                 client.PrintToConsole("[CCSMC DEBUG] " + information);
             }
-            Server.PrintToConsole("[CCSMC DEBUG] " + information);
-        }
+        } catch(Exception) {}
 
-        public static void LogTrace(string information) {
-        foreach(var client in Utilities.GetPlayers()) {
-            if(!client.IsValid || client.IsBot || client.IsHLTV)
-                continue;
-            
-            client.PrintToConsole("[CCSMC TRACE] " + information);
-        }
+        Server.PrintToConsole("[CCSMC DEBUG] " + information);
+    }
+
+    public static void LogTrace(string information) {
+        try {
+            foreach(var client in Utilities.GetPlayers()) {
+                if(!client.IsValid || client.IsBot || client.IsHLTV)
+                    continue;
+                
+                client.PrintToConsole("[CCSMC TRACE] " + information);
+            }
+        } catch(Exception) {}
+
         Server.PrintToConsole("[CCSMC TRACE] " + information);
-        }
+    }
     }
